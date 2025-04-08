820adbde08107a762e9a4381c0fcb451d0f63529
RCat021 <rinatsarafeev65@gmail.com>
2025-04-08 09:50:32 +0500

    counter

--- Scripts/Counter.cs
+++ Scripts/Counter.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using UnityEngine;
 using System;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class Counter : MonoBehaviour
 {
+
+    [SerializeField] private int _step = 1;
+    [SerializeField] private float _timerStep = 0.5f;
+
     public int CurrentCount { get; private set; } = 0;
+
+    private bool _isCounterActive = true;
+    private Coroutine _coroutine;
 
     public event Action OnCountChanged;
 
@@ -12,5 +20,26 @@ public class Counter : MonoBehaviour
     {
         CurrentCount += count;
         OnCountChanged?.Invoke();
+    }
+
+    public void ActiveCounter()
+    {
+        _coroutine = StartCoroutine(UpdateCounterRoutine());
+    }
+    public void DisactiveCounter()
+    {
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+    }
+
+    private IEnumerator UpdateCounterRoutine()
+    {
+        var waitDuration = new WaitForSeconds(_timerStep);
+
+        while (_isCounterActive)
+        {
+            IncreaseCount(_step);
+            yield return waitDuration;
+        }
     }
 }
--- Scripts/CounterToogle.cs
+++ Scripts/CounterToogle.cs
@@ -6,27 +6,25 @@ using UnityEngine;
 public class CounterToogle : MonoBehaviour
 {
     [SerializeField] private Counter _counter;
-    [SerializeField] private int _step = 1;
-    [SerializeField] private float _timerStep = 0.5f;
 
     private bool _isCounterActive = false;
 
-    public void ToggleCounter()
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ToggleCounter();
+        }
+    }
+
+    private void ToggleCounter()
     {
         _isCounterActive = !_isCounterActive;
 
         if (_isCounterActive)
-            StartCoroutine(UpdateCounterRoutine());
+            _counter.ActiveCounter();
         else
-            StopCoroutine(UpdateCounterRoutine());
-    }
-
-    private IEnumerator UpdateCounterRoutine()
-    {
-        while (_isCounterActive)
-        {
-            _counter.IncreaseCount(_step);
-            yield return new WaitForSeconds(_timerStep);
-        }
+            _counter.DisactiveCounter();
     }
 }
